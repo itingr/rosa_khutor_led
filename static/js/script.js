25de1e1e07b66dd0d1e7a39c96c4705f5f3ee3e5
itingr <borisov@in-gr.ru>
2021-07-06 12:51:59 +0300

    init

---
+++ static/js/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// generate current time format like '22:14'
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    const addText = checkSource(s);
+
+    // change clock's time
+    document.getElementById('clock').innerHTML = h + ":" + m;
+
+    // switch weather-avalanche
+    const weather = document.getElementsByClassName('weather')[0];
+    if (s < 30) {
+        weather.classList.add('visually-hidden');
+    } else {
+        weather.classList.remove('visually-hidden');
+    }
+    
+    var t = setTimeout(startTime, 1000);
+}
+
+// check current time's secconds to switch information blocks
+function checkSource(i) {
+    let txt = '';
+    if (i < 30) {
+        txt = ' before';
+    } else {
+        txt = ' after';
+    }
+    return txt;
+}
+
+//visually-hidden
+// add zero in front of numbers < 10
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
